e19232b4be94de46f761d742068414fcbce8bf0d
krazytristan <trstnjorge@gmail.com>
2025-07-02 23:10:29 +0800

    dashboard of the user

--- js/itdashboard.js
+++ js/itdashboard.js
@@ -14,22 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
   menuLinks.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
-
-      // Remove active class from all links
       menuLinks.forEach((l) => l.classList.remove("active"));
-
-      // Add active class to clicked link
       link.classList.add("active");
-
-      // Get target section ID
-      const target = link.getAttribute("data-link");
-
-      // Show the target section and hide others
+      const target = link.dataset.link;
       sections.forEach((sec) => {
         sec.classList.remove("active");
-        if (sec.id === target) {
-          sec.classList.add("active");
-        }
+        if (sec.id === target) sec.classList.add("active");
       });
     });
   });
